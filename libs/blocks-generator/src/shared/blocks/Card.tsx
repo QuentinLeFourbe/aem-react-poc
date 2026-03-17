9999c284a2bd8c085895bb50bd672c6c683e73ac
Quentin GARCIA <quentin.garcia@audemarspiguet.com>
2026-03-17 10:04:40 +0100

    add card

--- libs/blocks-generator/src/shared/blocks/Card.tsx
+++ libs/blocks-generator/src/shared/blocks/Card.tsx
@@ -1,3 +1,17 @@
-export function Card() {
-  return <div>Hello card</div>;
+import React from 'react';
+import './Card.css';
+
+interface CardProps {
+  title: string;
+  description: string;
 }
+
+export function Card({ title, description }: CardProps) {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
