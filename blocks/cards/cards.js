1ea9ef2ca482730c8b48f9867fc41c6afcb95782
Quentin GARCIA <quentin.garcia@audemarspiguet.com>
2026-03-17 09:07:28 +0100

    custom card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,17 +1,30 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.replaceChildren(ul);
+  /*
+   * Change: The block now treats the entire table as ONE card.
+   * Row 1: Title
+   * Row 2: Description
+   */
+  const titleRow = block.children[0];
+  const descRow = block.children[1];
+
+  const card = document.createElement('div');
+  card.className = 'card-content';
+
+  if (titleRow) {
+    const title = document.createElement('h3');
+    title.innerHTML = titleRow.innerHTML;
+    card.append(title);
+  }
+
+  if (descRow) {
+    const desc = document.createElement('div');
+    desc.className = 'card-description';
+    desc.innerHTML = descRow.innerHTML;
+    card.append(desc);
+  }
+
+  block.textContent = '';
+  block.append(card);
 }
